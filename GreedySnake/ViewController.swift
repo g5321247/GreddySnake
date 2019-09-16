b85ed983277c6b3bd950aa0cf130ec1240cac19e
劉峻岫 <g5321247@gmail.com>
2019-09-16 08:54:13 +0800

    Initial Commit

---
+++ GreedySnake/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  GreedySnake
+//
+//  Created by 劉峻岫 on 2019/9/16.
+//  Copyright © 2019 Addcn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
